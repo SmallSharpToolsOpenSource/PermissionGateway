5369f7bff7819819e4f2bd5b78d2b8724dcedc41
brennanMKE <brennan@smallsharptools.com>
2015-08-13 16:08:38 -0700

    removed unnecessary code from app delegate

--- PermissionGateway/AppDelegate.m
+++ PermissionGateway/AppDelegate.m
@@ -19,6 +19,9 @@
 
 @implementation AppDelegate
 
+#pragma mark - Application Lifecycle
+#pragma mark -
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
@@ -28,56 +31,11 @@
         [application registerForRemoteNotifications];
     }
     
-    
-    if (EnvVarIsTrue(@"ForceFirstLaunch")) {
-        DebugLog(@"Forcing first launch");
-    }
-    else {
-        DebugLog(@"Business as usual");
-    }
-
-    if (EnvVarIsTrue(@"DebugEnabled")) {
-        DebugLog(@"Debug is enabled");
-        DebugLog(@"username: %@", EnvVarString(@"DebugUsername"));
-        DebugLog(@"password: %@", EnvVarString(@"DebugPassword"));
-    }
-    else {
-        DebugLog(@"Debug is not enabled");
-    }
-    
-//    DebugLog(@"Locales: %@", [NSLocale availableLocaleIdentifiers]);
-    
-    NSLocale *locale = [NSLocale currentLocale];
-    NSString *countryCode = [locale objectForKey: NSLocaleCountryCode];
-    NSLocale *usLocale = [NSLocale localeWithLocaleIdentifier:@"en_US"];
-    NSString *country = [usLocale displayNameForKey: NSLocaleCountryCode value: countryCode];
-    
-    DebugLog(@"country: %@", country);
-    
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-}
+#pragma mark - Remote Notifications
+#pragma mark -
 
 - (void)application:(UIApplication *)application didRegisterUserNotificationSettings:(UIUserNotificationSettings *)notificationSettings {
     [[PGPermissionGateway sharedInstance] reportNotificationRegisteredWithSettings:notificationSettings];
